d18fdcc8a575e7a4c0f81c516e6e1d5c059e8c72
antoninpaoli <53855385+antoninpaoli@users.noreply.github.com>
2020-05-21 16:11:23 +0200

    Test2

---
+++ ile_interdite/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    public boolean NellyEstTuLa() {
+        //return true;
+        return false;
+    }
+
+    public boolean AntoineEstTuLa() {
+        //return true;
+        return false;
+    }
+}

da961ac69e7e389d03368e91ab8fbce4a8c456df
Antoine B <antoinebarbannaud@gmail.com>
2020-05-21 19:10:10 +0200

    renamed Coord

--- ile_interdite/src/Model/Island.java
+++ ile_interdite/src/Model/Island.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 import java.util.Observable;
 
 public class Island extends Observable {
-    public HashMap<Location, Cell> board;
+    public HashMap<Coord, Cell> board;
     public int width;
     public int height;
 
     public Island(int w, int h){
         this.width = w;
         this.height = h;
-        this.board = new HashMap<Location, Cell>();
+        this.board = new HashMap<Coord, Cell>();
 
         //helicoptere 1chance sur width*height
 //      //keys on va en avoir 4
@@ -20,7 +20,7 @@ public class Island extends Observable {
         //nous remplissons la hashmap avec toutes les cellules du plateau
         for(int i = 0; i < this.width; i++){
             for(int j = 0; j < this.height; j++){
-                Location l = new Location(i,j);
+                Coord l = new Coord(i,j);
                 Cell c =
                 board.put(l, Cell);
             }

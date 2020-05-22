2c20b2606cac11176ff7437148adcd134782b6a9
Nelly <nellylam94@gmail.com>
2020-05-22 10:51:18 +0200

    add player suite2

--- ile_interdite/src/Model/Player.java
+++ ile_interdite/src/Model/Player.java
@@ -1,8 +1,9 @@
-package Model;
+/*package Model;
 
 import java.util.ArrayList;
 
 public class Player {
+
     protected static int idPlayer = 0;
     private Coord location;
     private ArrayList<Cell.Element> key;
@@ -15,9 +16,13 @@ public class Player {
         this.isDead = false;
     }
 
+ */
+
+
     /**
      * méthode initPlayer() : initialise le player sur un island, sur une cell hélicoptère
      */
+    /*
     public void initPlayer(){
     }
 
@@ -25,6 +30,7 @@ public class Player {
      * méthode getIdJ() : donne l'indice du joueur
      * @return l'indice int idPlayer du joueur
      */
+    /*
     public int getIdJ() {
         return this.idPlayer;
     }
@@ -40,23 +46,27 @@ public class Player {
      * méthode addkey() : ajoute une clé (élément) à la liste des key du player
      * @param : un Element e
      */
+    /*
     public void addKey(Cell.Element e){
         this.key.add(e);
     }
-
+*/
     /**
      * méthode addArtifact() : ajoute un artefact à la liste des artifact du player
      * @param : un Element e
      */
+    /*
     public void addArtifact(Cell.Element e){
         this.artifact.add(e);
     }
+    */
 
     /**
      * méthode cellAroundPlayer() : donne une liste des cell (haut, bas, droite, gauche) situées autour du player
      * @param : un Island island
      * @return une ArrayList<Cell> cap
      */
+    /*
     public ArrayList<Cell> cellAroundPlayer(Island island){
         ArrayList<Cell> cap = new ArrayList<Cell>();
         Coord haut = new Coord(this.location.getX(), this.location.getY()+1);
@@ -69,13 +79,14 @@ public class Player {
         cap.add(island.board.get(gauche));
         return cap;
     }
-
+*/
     /**
      * méthode die() : update le statut du player isDead en true si :
      * - le player se situe sur une cell submergée
      * - le player est entouré de cells submergées
      * @param : un Island island (board)
      */
+    /*
     public void die(Island island){
         ArrayList<Cell> cap = cellAroundPlayer(island);
         int compteur = 0;
@@ -98,3 +109,4 @@ public class Player {
     //dryZone(Cellule c) (position + droite gauche haut bas)
 
 }
+*/

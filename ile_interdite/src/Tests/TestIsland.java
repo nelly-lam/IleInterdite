3f9be7cc7966eb3e710bdbba5f9d6b5472cdbd11
Antoine B <antoinebarbannaud@gmail.com>
2020-05-22 18:51:23 +0200

    implemented Island & Player unit tests

--- ile_interdite/src/Tests/TestIsland.java
+++ ile_interdite/src/Tests/TestIsland.java
@@ -1,13 +1,32 @@
 package Tests;
 
+import Model.Cell;
+import Model.Island;
 import org.junit.jupiter.api.Test;
 
 public class TestIsland {
+
     @Test
-    void getCell() {
+    void getCellTest() {
+        Island island = new Island(20, 20);
+        Cell c = new Cell(false, Cell.Element.Air, Cell.Element.None);
+        island.board[0][0] = c;
+        assert (island.getCell(0, 0) == c);
     }
 
     @Test
-    void risingWater() {
+    void risingWaterTest() {
+        Island board = new Island(10, 10);
+        board.risingWater();
+        int compteur = 0;
+        for (int i = 0; i < board.width; i++) {
+            for (int j = 0; j < board.height; j++) {
+                if (board.getCell(i, j).state == Cell.State.Flooded) {
+                    compteur++;
+                }
+            }
+        }
+        assert (compteur == 3);
     }
+
 }
--- ile_interdite/src/Tests/TestPlayer.java
+++ ile_interdite/src/Tests/TestPlayer.java
@@ -1,41 +1,63 @@
 package Tests;
 
+import Model.Cell;
+import Model.Island;
+import Model.Player;
 import org.junit.jupiter.api.Test;
 
 public class TestPlayer {
+
     @Test
-    void initPlayer() {
+    void getNameTest() {
+        Island island = new Island(20,20);
+        Player p = new Player(island, "Joueur", 10, 10);
+        assert (p.getName() == "Joueur");
     }
 
     @Test
-    void getIdJ() {
+    void getAbsTest() {
+        Island island = new Island(20,20);
+        Player p = new Player(island, "Joueur", 1, 2);
+        assert (p.getAbs() == 1);
     }
 
     @Test
-    void getName() {
+    void getOrdTest() {
+        Island island = new Island(20,20);
+        Player p = new Player(island, "Joueur", 1, 2);
+        assert (p.getOrd() == 2);
     }
 
     @Test
-    void getAbs() {
+    void addKeyTest() {
+        Island island = new Island(20,20);
+        Player p = new Player(island, "Joueur", 1, 2);
+        p.addKey(Cell.Element.Fire);
+        //TODO add getKey
+        for(int i = 0; i < p. )
     }
 
     @Test
-    void getOrd() {
+    void addArtifactTest() {
+        Island island = new Island(20,20);
+        Player p = new Player(island, "Joueur", 1, 2);
+        p.addArtifact(Cell.Element.Fire);
+        //TODO add getArtifact
+        for(int i = 0; i < p.)
     }
 
     @Test
-    void addKey() {
-    }
+    void nearbyCellsTest() {
 
-    @Test
-    void addArtifact() {
-    }
-
-    @Test
-    void cellAroundPlayer() {
     }
 
     @Test
     void die() {
+        Island island = new Island(20,20);
+        Player p = new Player(island, "Joueur", 1, 2);
+        p.die();
+        //TODO add getIsDead
+        assert (p.isDead);
+
     }
 }

74c2b6a058f84462c8b7a1fb67c939d445bd7c41
antoninpaoli <53855385+antoninpaoli@users.noreply.github.com>
2020-05-22 11:40:45 +0200

    player

--- ile_interdite/src/Model/Player.java
+++ ile_interdite/src/Model/Player.java
@@ -1,43 +1,42 @@
-/*package Model;
+package Model;
 
 import java.util.ArrayList;
 
 public class Player {
-
+    private String name;
     protected static int idPlayer = 0;
+    private Island island;
     private Coord location;
     private ArrayList<Cell.Element> key;
     private ArrayList<Cell.Element> artifact;
     private boolean isDead;
 
-    public Player(Coord l){
+    public Player(String name, Coord l){
+        this.name = name;
         this.idPlayer++;
         this.location = l;
         this.isDead = false;
     }
 
- */
-
 
     /**
-     * méthode initPlayer() : initialise le player sur un island, sur une cell hélicoptère
+     * méthode initPlayer() : initialise le player sur un island, sur la cell du milieu
      */
-    /*
     public void initPlayer(){
+        this.location = new Coord(this.island.width/2, this.island.height/2);
     }
 
     /**
      * méthode getIdJ() : donne l'indice du joueur
      * @return l'indice int idPlayer du joueur
      */
-    /*
     public int getIdJ() {
         return this.idPlayer;
-    }
-
-    //EN CHANTIER
+    }/*
+    //EN CH
+    /*ANTIER
     public void updatePosition(Island island, Cell c){
-        //this.location = new Coord(island.board.get(c));
+        //this.locathis.tion = new Coord(island.board.get(c));
     } //changer la position du joueur
 
     //isMovementPossible(Cellule c) // checker si le joueur a le droit de faire un mouvement
@@ -57,12 +56,13 @@ public class Player {
      */
     /*
     public void addArtifact(Cell.Element e){
-        this.artifact.add(e);
+        this.artifact.add(e) USING HASHMAP;
     }
     */
 
     /**
-     * méthode cellAroundPlayer() : donne une liste des cell (haut, bas, droite, gauche) situées autour du player
+     * méthode cellAroundPlayer() : donne une liste des cell (haut
+     * , bas, droite, gauche) situées autour du player
      * @param : un Island island
      * @return une ArrayList<Cell> cap
      */
@@ -72,12 +72,28 @@ public class Player {
         Coord haut = new Coord(this.location.getX(), this.location.getY()+1);
         Coord bas = new Coord(this.location.getX(), this.location.getY()-1);;
         Coord droite = new Coord(this.location.getX()+1, this.location.getY());;
-        Coord gauche = new Coord(this.location.getX()-1, this.location.getY());;
-        cap.add(island.board.get(haut));
-        cap.add(island.board.get(bas));
-        cap.add(island.board.get(droite));
+        Coord gauche = new Coord(this.location.getX()-1, this.location.getthis.Y());;
+        cap.add(islanthis.d.board.get(haut));
+        cap.add(islathis.nd.board.get(bas));
+        cap.add(island.this.board.get(droite));
         cap.add(island.board.get(gauche));
-        return cap;
+
+    public ArrayList<Cell> cellAroundPlayer
+   ArrayList<Cell> cap = new ArrayList<Cell>();
+        int playerX = this.location.getX();
+        int playerY = this.location.getY();
+        if (playerX != 0) {
+            cap.add(this.island.board[playerX][playerY-1]);
+        }
+        if (playerX != this.island.height) {
+            cap.add(this.island.board[playerX][playerY+1]);
+        }
+        Coord right = new Coord(this.location.getX()+1, this.location.getY());
+        Coord left = new Coord(this.location.getX()-1, this.location.getY());
+        cap.add(up);
+        return cap; (){
+
+    } USING HASHMAP   return cap;
     }
 */
     /**
@@ -89,7 +105,7 @@ public class Player {
     /*
     public void die(Island island){
         ArrayList<Cell> cap = cellAroundPlayer(island);
-        int compteur = 0;
+       this. int compteur = 0;
         if (island.board.get(this.location).state == Cell.State.Submerged){ //si la cell où se trouve le player est submergée
             System.out.println("Mskn you are dead");
             this.isDead = true;
@@ -102,11 +118,9 @@ public class Player {
             if (compteur == cap.size()){ //si toutes les cells de cap sont submergées
                 System.out.println("Mskn you are dead");
                 this.isDead = true;
-            }
-        }
-     }
+        */ //   }
+       // }
+     //}
 
     //dryZone(Cellule c) (position + droite gauche haut bas)
-
 }
-*/

dc25d105d688dd1b2bd14a15df7356cd7521dcc0
Antoine B <antoinebarbannaud@gmail.com>
2020-05-22 17:01:56 +0200

    implemented Cell unit tests

--- ile_interdite/src/Tests/TestCell.java
+++ ile_interdite/src/Tests/TestCell.java
@@ -1,32 +1,73 @@
 package Tests;
+import Model.Cell;
 import org.junit.jupiter.api.Test;
 
 public class TestCell {
     @Test
-    void hasKey() {
+    void hasKeyTest() {
+        Cell c = new Cell(false, Cell.Element.Air, Cell.Element.None);
+        assert (c.hasKey());
+        Cell c2 = new Cell(false, Cell.Element.None, Cell.Element.None);
+        assert (!c2.hasKey());
     }
 
     @Test
-    void hasArtifact() {
+    void hasArtifactTest() {
+        Cell c = new Cell(false, Cell.Element.None, Cell.Element.Air);
+        assert (c.hasArtifact());
+        Cell c2 = new Cell(false, Cell.Element.None, Cell.Element.None);
+        assert (!c2.hasArtifact());
     }
 
     @Test
-    void isHelicopter() {
+    void isHelicopterTest() {
+        Cell c = new Cell(true, Cell.Element.None, Cell.Element.None);
+        assert (c.isHelicopter());
+        Cell c2 = new Cell(false, Cell.Element.None, Cell.Element.None);
+        assert (!c2.isHelicopter());
     }
 
     @Test
-    void updateKey() {
+    void updateKeyTest() {
+        Cell c = new Cell(false, Cell.Element.None, Cell.Element.Air);
+        c.updateKey();
+        assert (c.getKey() == Cell.Element.None);
+
+        Cell c2 = new Cell(false, Cell.Element.None, Cell.Element.None);
+        c2.updateKey();
+        assert (c.getKey() == Cell.Element.None);
     }
 
     @Test
-    void updateArtifact() {
+    void updateArtifactTest() {
+        Cell c = new Cell(false, Cell.Element.None, Cell.Element.Air);
+        c.updateArtifact();
+        assert (c.getArtifact() == Cell.Element.None);
+
+        Cell c2 = new Cell(false, Cell.Element.None, Cell.Element.None);
+        c2.updateArtifact();
+        assert (c2.getArtifact() == Cell.Element.None);
     }
 
     @Test
-    void flood() {
+    void floodTest() {
+        Cell c = new Cell(false, Cell.Element.None, Cell.Element.None);
+        assert (c.state == Cell.State.Normal);
+        c.flood();
+        assert (c.state == Cell.State.Flooded);
+        c.flood();
+        assert (c.state == Cell.State.Submerged);
+        c.flood();
+        assert (c.state == Cell.State.Submerged);
     }
 
     @Test
-    void isSubmerged() {
+    void isSubmergedTest() {
+        Cell c = new Cell(false, Cell.Element.None, Cell.Element.None);
+        Cell c2 = new Cell(false, Cell.Element.None, Cell.Element.None);
+        c.flood();
+        c.flood();
+        assert(c.isSubmerged());
+        assert(!c2.isSubmerged());
     }
 }

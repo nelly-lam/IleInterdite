38630e7b80d99587616ce72cfb4e26ab03200234
antoninpaoli <53855385+antoninpaoli@users.noreply.github.com>
2020-06-02 15:25:09 +0200

    Résolution problème de font2

--- ile_interdite/src/fonts/PantonFont.java
+++ ile_interdite/src/fonts/PantonFont.java
@@ -11,7 +11,7 @@ public class PantonFont {
 
     public static Font getPanton() {
         try {
-            FileInputStream filePanton = new FileInputStream("src/fonts/Panton-Regular.ttf");
+            FileInputStream filePanton = new FileInputStream("src/bl/fonts/Panton-Regular.ttf");
             return Font.createFont(Font.TRUETYPE_FONT, filePanton);
         } catch (FontFormatException | IOException e) {
             return new Font("Arial", Font.PLAIN, 15);
@@ -20,16 +20,16 @@ public class PantonFont {
 
     public static Font getPantonLight() {
         try {
-            InputStream filePantonLight = new FileInputStream("src/fonts/Panton-Light.ttf");
+            InputStream filePantonLight = new FileInputStream("src/bl/fonts/Panton-Light.ttf");
             return Font.createFont(Font.TRUETYPE_FONT, filePantonLight);
         } catch (FontFormatException | IOException e) {
-            return Font.getFont("Arial");
+            return new Font("Arial", Font.PLAIN, 15);
         }
     }
 
     public static Font getPantonBold() {
         try {
-            FileInputStream filePantonBold = new FileInputStream("src/fonts/Panton-Bold.ttf");
+            FileInputStream filePantonBold = new FileInputStream("src/bl/fonts/Panton-Bold.ttf");
             return Font.createFont(Font.TRUETYPE_FONT, filePantonBold);
         } catch (FontFormatException | IOException e) {
             return new Font("Arial", Font.BOLD, 15);
